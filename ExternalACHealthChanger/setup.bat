e1a1b691cbc222be503ede015ad367b8a5854d11
DraxFM <79086740+DraxFM@users.noreply.github.com>
2023-04-11 15:04:43 +0200

    Add files via upload

---
+++ ExternalACHealthChanger/setup.bat
@@ -0,0 +1,4 @@
+@echo off
+title Installing requirements - This might take a few minutes
+echo DO NOT CLOSE WINDOW! PROGRAM CLOSES AUTOMATICALLY!
+pip install -r requirements.txt
---
+++ ExternalACHealthChanger/start.bat
@@ -0,0 +1,54 @@
+@echo off
+title Assault Cube Health Changer by DraxFM aka. Drax
+:main
+cls
+echo.
+echo Simple Assault Cube adress manipulator.
+echo.
+echo Github: www.github.com/DraxFM
+echo Discord: Drax#5934
+echo.
+echo Options:
+echo [A] Health Changer
+echo [B] Godmode
+echo [C] Help
+echo [D] Join my Discord server!
+echo.
+set /p press=Command: 
+if /i %press%==A goto hc
+if /i %press%==B goto gm
+if /i %press%==C goto help
+if /i %press%==D goto invite
+echo.
+echo %press% is not an available command!
+echo Press anything to continue...
+pause > nul
+goto main
+exit
+
+:hc
+cls
+python health.py
+pause > nul
+exit
+
+:gm
+cls
+python godmode.py
+pause > nul
+exit
+
+:help
+cls
+echo.
+echo This page is pretty empty as of right now but if you have problems with the health manipulator, be sure to play in an active match. The manipulator does not work in the lobby!
+echo.
+echo Press any key to continue...
+pause > nul
+goto main
+
+:invite
+cls
+echo.
+echo Redirecting to Discord server...
+start https://discord.gg/sEXECdC3Et
